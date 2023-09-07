5a525f90f037f458779822a2e41634df292989df
Elsie Urena <71279445+elsieu9700@users.noreply.github.com>
2023-09-07 10:30:53 -0400

    Create Maps.r

---
+++ R/Graphs/Maps.r
@@ -0,0 +1,13 @@
+# install.packages("maps")
+
+# Read the dataset and convert it into
+# Dataframe
+data <- read.csv("worldcities.csv")
+df <- data.frame(data)
+  
+# Load the required libraries
+library(maps)
+map(database = "world")
+    
+# marking points on the map
+points(x = df$lat[1:500], y = df$lng[1:500], col = "Red")
